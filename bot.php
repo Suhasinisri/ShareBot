603a17b06c1ab7825a7d7328940ebeed045d1b87
Alex <rabbitx98@gmail.com>
2020-04-19 23:53:31 +0430

    fix showing msg

--- bot.php
+++ bot.php
@@ -6,6 +6,6 @@ include "config.php";
 $link = isset($_REQUEST['link'])?$_REQUEST['link']:null;
 
 if (!is_null($link)){
-    echo send_link($bot,$link,$content);
+    print_r(send_link($bot,$link,$content));
     die();
 }
