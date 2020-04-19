f845ea023d91e6c2218e68da300270ee184152e1
Alex <rabbitx98@gmail.com>
2020-04-20 00:01:26 +0430

    checking

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@ include "config.php";
 $link = isset($_REQUEST['link']) ? $_REQUEST['link'] : null;
 
 if (!is_null($link)) {
-     json_decode(send_link($bot, $link, $content), JSON_PRETTY_PRINT);
+     var_dump(send_link($bot, $link, $content));
 
     die();
 }
