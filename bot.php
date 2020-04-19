051140ef11f222f0702d864b94f7d3f89740adb4
Alex <rabbitx98@gmail.com>
2020-04-20 00:09:45 +0430

    fixing showing json FIXED

--- bot.php
+++ bot.php
@@ -7,7 +7,7 @@ $link = isset($_REQUEST['link']) ? $_REQUEST['link'] : null;
 
 if (!is_null($link)) {
     header('Content-Type: application/json');
-     echo json_encode(send_link($bot, $link, $content),JSON_PRETTY_PRINT,JSON);
+     echo json_encode(send_link($bot, $link, $content),JSON_PRETTY_PRINT);
 
     die();
 }
