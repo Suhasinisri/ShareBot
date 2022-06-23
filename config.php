abac275eb925524228bae32bb203d004981e52dd
suhasinivas <108001656+Suhasinisri@users.noreply.github.com>
2022-06-23 10:04:17 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,15 +3,15 @@ require "Telegram.php";
 require "TelegramErrorLogger.php";
 $configFile = file_get_contents(__DIR__ . '/cereditional.json');
 $config = json_decode($configFile);
-$token = $config->token;
-$chnl_id = $config->channel_id;
+$token = $config->5359458897:AAGwwm_vpw7tJk-gtb0AgC_DF29fhcswnZk;
+$chnl_id = $config->5348405228;
 
 define("TOKEN", $token);
 define("CHANNEL_ID", $chnl_id);
 
-$bot = new Telegram(TOKEN);
+$bot = new Telegram(5359458897:AAGwwm_vpw7tJk-gtb0AgC_DF29fhcswnZk);
 $content = array(
-    'chat_id' => CHANNEL_ID,
+    'chat_id' =>5348405228,
     'text' => '',
     'disable_notification' => true
 );
